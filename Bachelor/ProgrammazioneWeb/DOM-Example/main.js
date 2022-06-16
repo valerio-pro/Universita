78ee81c8c04afc69c4ba38befa6713ae42e33a2e
Valerio Pro <76836755+valerio-pro@users.noreply.github.com>
2022-06-16 12:28:09 +0200

    Update main.js

--- Bachelor/ProgrammazioneWeb/DOM-Example/main.js
+++ Bachelor/ProgrammazioneWeb/DOM-Example/main.js
@@ -6,8 +6,24 @@ const closeBtn = document.getElementById("close-btn");
 const divOverlay = document.getElementById("div-overlay");
 let data;
 
-// ESERCIZIO 2
-addEventListener("load", async () => {
+// <node>.parentNode (nodo padre)
+// <node>.nextElementSibling (nodo fratello successivo)
+// <event>.target (nodo su cui e' definito l'evento) -> l'oggetto <event> e' preso in input dalle callback degli eventi
+
+// <node>.classList.add("<css-class-name>")
+// <node>.classList.remove("<css-class-name>")
+// <node>.classList.toggle("<css-class-name>") -> aggiunge la classe se non e' presente, rimuove la classe se e' presente
+
+// <node>.setAttribute("<attribute-name>", "<value>")
+// <node>.innerHTML = "<string-of-text>"
+
+// Se il nodo DOM e' un elemento HTML "<input ...>" di un Form, allora con <node>.value si accede al valore assegnato all'elemento "<input ...>"
+// Se si definisce una callback per un evento "submit" su un Form, allora con <event>.preventDefault() si evita il ricarimento della pagina
+// quando e' premuto il bottone di "submit"
+
+
+// Le callback degli eventi hanno in input l'oggetto "event" -> event.target e' il nodo DOM sul quale si sta definendo l'evento
+addEventListener("load", async (event) => {
 
     let response = await fetch("./data.json");
     data = await response.json();
@@ -27,8 +43,7 @@ addEventListener("load", async () => {
 });
 
 
-// ESERCIZIO 5
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", (event) => {
 
     if (window.innerWidth < 600) {
 
@@ -38,8 +53,7 @@ menuBtn.addEventListener("click", () => {
 
 });
 
-// ESERCIZIO 7
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener("click", (event) => {
 
     divOverlay.classList.add("hidden");
 
